cebb62a360acc923e333bfac8f2b02abf5daf644
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-05 22:23:59 +0300

    implement comments code

--- examples/blog/src/App/Comments/Page.elm
+++ examples/blog/src/App/Comments/Page.elm
@@ -3,7 +3,7 @@ module App.Comments.Page exposing (..)
 import Api exposing (Api, HttpMethod(..))
 import App.Comments.Form as CommentsForm exposing (Form)
 import App.Config exposing (..)
-import Data.Post exposing (Post)
+import Data.Comment exposing (Comment)
 import FormState exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -12,36 +12,44 @@ import Json.Decode as Json exposing (field)
 import Update.Deep exposing (..)
 
 type Msg
-  = SetPost (Maybe Post)
+  = ApiMsg (Api.Msg Comment)
   | FormMsg (FormState.Msg Form)
 
 type alias State =
-  { post : Maybe Post
-  , form : FormState Form }
+  { comment : Api Comment
+  , form    : FormState Form }
 
 init : Config -> Init State Msg
 init { flags } =
-  let form = FormState.init CommentsForm.fields
+  let comment = Api.init { endpoint = flags.api ++ "/posts"
+                         , method   = HttpPost
+                         , decoder  = Json.field "comment" Data.Comment.decoder }
+      form = FormState.init CommentsForm.fields
                  { email   = ""
                  , comment = "" }
-   in { post = Nothing
-      , form = form.state }
+   in { comment = comment.state
+      , form    = form.state }
         |> initial
         |> initCmd FormMsg form
+        |> initCmd ApiMsg comment
 
 update : Msg -> State -> Update State Msg a
 update msg state =
   let resetForm = update (FormMsg FormState.Reset)
       default = Api.defaultHandlers
    in case msg of
+        ApiMsg apiMsg ->
+          state.comment
+            |> Api.update { default | onSuccess = resetForm } apiMsg
+            |> andThen (\comment -> save { state | comment = comment })
+            |> mapCmd ApiMsg
+            |> consumeEvents
         FormMsg formMsg ->
           state.form
             |> FormState.update defaultHandlers formMsg
             |> andThen (\form -> save { state | form = form })
             |> mapCmd FormMsg
             |> consumeEvents
-        SetPost post ->
-          save { state | post = post }
 
 subscriptions : State -> Sub Msg
 subscriptions _ = Sub.none
