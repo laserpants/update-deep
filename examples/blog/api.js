95020dbecd86ac5405ee2ef96aae6fc9aa808f5a
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-05 14:43:17 +0300

    update js code

---
+++ examples/blog/api.js
@@ -0,0 +1,110 @@
+var users =
+[
+  {
+    id: 1,
+    name: 'Mr. Test',
+    password: 'test',
+    email: 'test@test.com',
+    login: 'test'
+  }
+];
+
+var posts =
+[
+  {
+    id: 1,
+    title: '1',
+    body: 'hello',
+    comments: []
+  }
+];
+
+var postsId = 1;
+
+xhook.before(function(request, callback) {
+
+  if (request.url.endsWith('auth/register') && 'POST' === request.method) {
+    setTimeout(function() {
+      callback({
+        status: 200,
+        data: JSON.stringify({ status: 'success' }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }, 800);
+  }
+  else if (request.url.endsWith('auth/login') && 'POST' === request.method) {
+    setTimeout(function() {
+      var params = JSON.parse(request.body);
+      var filtered = users.filter(function(user) {
+        return user.login === params.login && user.password === params.password;
+      });
+      if (filtered.length > 0) {
+        var response = filtered[0];
+        response.token = 'fake-jwt-token';
+        callback({
+          status: 200,
+          data: JSON.stringify({ user: response }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      } else {
+        console.log('401 Unauthorized');
+        callback({
+          status: 401,
+          data: JSON.stringify({ error: 'Unauthorized' }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }
+    }, 800);
+  } else if (request.url.endsWith('posts')) {
+    if ('GET' === request.method) {
+      setTimeout(function() {
+        callback({
+          status: 200,
+          data: JSON.stringify({ posts: posts }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }, 800);
+    } else if ('POST' === request.method) {
+      setTimeout(function() {
+        var post = JSON.parse(request.body);
+        post.id = ++postsId;
+        post.comments = [];
+        posts.push(post);
+        callback({
+          status: 200,
+          data: JSON.stringify({ post: post }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }, 800);
+    }
+  } else if (/posts\/\d+$/.test(request.url) && 'GET' === request.method) {
+    setTimeout(function() {
+      var id = request.url.match(/posts\/(\d+)$/)[1];
+      var filtered = posts.filter(function(post) { return post.id == id; });
+      if (filtered.length > 0) {
+        var response = filtered[0];
+        callback({
+          status: 200,
+          data: JSON.stringify({ post: response }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      } else {
+        console.log('Not Found');
+        callback({
+          status: 404,
+          data: JSON.stringify({ error: 'Not Found' }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }
+    }, 800);
+  } else if (request.url.endsWith('comments') && 'POST' === request.method) {
+
+    callback();
+
+  } else {
+
+    callback();
+
+  }
+
+});
--- examples/blog/index.js
+++ examples/blog/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('./index.html');
+require('./api.js');
 
 var Elm = require('./src/Main.elm').Elm;
 
@@ -9,84 +10,4 @@ var app = Elm.Main.init({
   flags: {
     api: process.env.API_URL || 'http://localhost:4000'
   }
-});
-
-// API
-
-var users =
-[
-  {
-    id: 1,
-    name: 'Mr. Test',
-    password: 'test',
-    email: 'test@test.com',
-    login: 'test'
-  }
-];
-
-var posts =
-[
-  {
-    id: 1,
-    title: '1',
-    body: 'hello',
-    comments: []
-  }
-];
-
-var postsId = 1;
-
-xhook.before(function(request, callback) {
-
-  if (request.url.endsWith('auth/login') && 'POST' === request.method) {
-    setTimeout(function() {
-      var params = JSON.parse(request.body);
-      var filtered = users.filter(function(user) {
-        return user.login === params.login && user.password === params.password;
-      });
-      if (filtered.length > 0) {
-        var response = filtered[0];
-        response.token = 'fake-jwt-token';
-        callback({
-          status: 200,
-          data: JSON.stringify({ user: response }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-      } else {
-        console.log('401 Unauthorized');
-        callback({
-          status: 401,
-          data: JSON.stringify({ error: 'Unauthorized' }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }
-    }, 800);
-  } else if (request.url.endsWith('posts')) {
-    if ('GET' === request.method) {
-      setTimeout(function() {
-        callback({
-          status: 200,
-          data: JSON.stringify({ posts: posts }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }, 800);
-    } else if ('POST' === request.method) {
-      setTimeout(function() {
-        var post = JSON.parse(request.body);
-        post.id = ++postsId;
-        post.comments = [];
-        posts.push(post);
-        callback({
-          status: 200,
-          data: JSON.stringify({ post: post }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }, 800);
-    }
-  } else {
-
-    callback();
-
-  }
-
 });
