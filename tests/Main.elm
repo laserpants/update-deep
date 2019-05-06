2637b3e901d82e9937f90f3e2904ead397a2cf7a
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-07 00:47:21 +0300

    build out tests

--- tests/Main.elm
+++ tests/Main.elm
@@ -8,7 +8,8 @@ import Update.Deep exposing (..)
 testSave : Test
 testSave =
   let state = 5
-      ( a, _, e ) = save state
+      ( a, _, e ) =
+        save state
    in describe "save"
         [ test "state" <|
             \_ -> Expect.equal a state
@@ -18,14 +19,22 @@ testSave =
 testMap : Test
 testMap =
   let state = 5
-      ( a, _, e ) = map (\x -> x + 1) (save state)
+      ( a, _, e ) =
+        map (\x -> x + 1) (save state)
    in describe "map"
         [ test "this" <|
             \_ -> Expect.equal a (state + 1) ]
+
+testJoin : Test
+testJoin =
+  let ( a, _, e ) =
+        join (save (save 5))
+   in describe "join"
+        [ test "this" <|
+            \_ -> Expect.equal a 5 ]
 
 suite : Test
 suite =
   describe "Update Deep"
     [ testSave
-    , testMap
-    ]
+    , testMap ]
