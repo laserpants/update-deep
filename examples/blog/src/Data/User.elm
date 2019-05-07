1410dea289390823ccc71d93bc0b1b29b00866eb
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-07 14:06:21 +0300

    add id field to User

--- examples/blog/src/Data/User.elm
+++ examples/blog/src/Data/User.elm
@@ -3,13 +3,15 @@ module Data.User exposing (..)
 import Json.Decode as Json exposing (field)
 
 type alias User =
-  { email : String
+  { id    : Int
+  , email : String
   , login : String
   , name  : String }
 
 decoder : Json.Decoder User
 decoder =
-  Json.map3 User
+  Json.map4 User
+    (field "id"    Json.int)
     (field "email" Json.string)
     (field "login" Json.string)
     (field "name"  Json.string)
