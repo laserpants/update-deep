aeaa35b56e23718caf6836ac09d8ee7a2e617e8c
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-08 19:36:41 +0300

    refactor

--- examples/blog/src/Main.elm
+++ examples/blog/src/Main.elm
@@ -1,16 +1,55 @@
 module Main exposing (..)
 
-import App exposing (Msg(..), State)
-import App.Router
-import Flags exposing (..)
-import Update.Deep.Browser as Deep
+import Browser
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation as Navigation
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Url exposing (Url)
 
-main : Program Flags State Msg
+type Page
+  = HomePage
+  | AboutPage
+  | PostCreatePage
+
+--
+
+type Msg
+  = UrlChange Url
+  | UrlRequest UrlRequest
+
+type alias Flags = ()
+
+type alias Model =
+  { page : Page }
+
+init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags url key = ( { page = HomePage }, Cmd.none )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model = 
+  Sub.none
+
+view : Model -> Document Msg
+view model =
+  { title = ""
+  , body = 
+    [ div [] 
+      [ text "Hello" ]
+    ]
+  }
+
+main : Program Flags Model Msg
 main =
-  Deep.application
-    { init          = App.init
-    , update        = App.update
-    , subscriptions = App.subscriptions
-    , view          = App.view
-    , onUrlChange   = RouterMsg << App.Router.UrlChange
-    , onUrlRequest  = RouterMsg << App.Router.UrlRequest }
+  Browser.application
+    { init          = init
+    , update        = update
+    , subscriptions = subscriptions
+    , view          = view
+    , onUrlChange   = UrlChange
+    , onUrlRequest  = UrlRequest }
