cb6ab3fa0302eea4146d4359db757e01cc2a52c5
Johannes Hildén <hildenjohannes@gmail.com>
2019-05-18 16:42:14 +0300

    run elm format

--- src/Update/Deep.elm
+++ src/Update/Deep.elm
@@ -1,66 +1,122 @@
-module Update.Deep exposing (..)
+module Update.Deep exposing (Update, andFinally, andInvokeHandler, andRunCmd, andThen, ap, applicationInit, documentInit, foldEvents, invokeHandler, join, kleisli, map, map2, map3, map4, map5, map6, mapCmd, runCmd, runUpdate, save)
 
-type alias Update m c e = ( m, Cmd c, List e )
+
+type alias Update m c e =
+    ( m, Cmd c, List e )
+
 
 save : m -> Update m c e
-save model = ( model, Cmd.none, [] )
+save model =
+    ( model, Cmd.none, [] )
+
 
 runCmd : Cmd c -> m -> Update m c e
-runCmd cmd state = ( state, cmd, [] )
+runCmd cmd state =
+    ( state, cmd, [] )
+
 
 mapCmd : (c -> d) -> Update m c e -> Update m d e
-mapCmd f ( model, cmd, events ) = ( model, Cmd.map f cmd, events )
+mapCmd f ( model, cmd, events ) =
+    ( model, Cmd.map f cmd, events )
+
 
 invokeHandler : e -> m -> Update m c e
-invokeHandler handler state = ( state, Cmd.none, [ handler ] )
+invokeHandler handler state =
+    ( state, Cmd.none, [ handler ] )
+
 
 ap : Update (a -> b) c e -> Update a c e -> Update b c e
-ap ( f, cmda, e ) ( model, cmdb, e2 ) = ( f model, Cmd.batch [ cmda, cmdb ], e ++ e2 )
+ap ( f, cmda, e ) ( model, cmdb, e2 ) =
+    ( f model, Cmd.batch [ cmda, cmdb ], e ++ e2 )
+
 
 map : (a -> b) -> Update a c e -> Update b c e
-map f ( model, cmd, events ) = ( f model, cmd, events )
+map f ( model, cmd, events ) =
+    ( f model, cmd, events )
+
 
 map2 : (a -> b -> p) -> Update a c e -> Update b c e -> Update p c e
-map2 f = ap << map f
+map2 f =
+    ap << map f
+
 
 map3 : (a -> b -> p -> q) -> Update a c e -> Update b c e -> Update p c e -> Update q c e
-map3 f x = ap << map2 f x
+map3 f x =
+    ap << map2 f x
+
 
 map4 : (a -> b -> p -> q -> r) -> Update a c e -> Update b c e -> Update p c e -> Update q c e -> Update r c e
-map4 f x y = ap << map3 f x y
+map4 f x y =
+    ap << map3 f x y
+
 
 map5 : (a -> b -> p -> q -> r -> s) -> Update a c e -> Update b c e -> Update p c e -> Update q c e -> Update r c e -> Update s c e
-map5 f x y z = ap << map4 f x y z
+map5 f x y z =
+    ap << map4 f x y z
+
 
 map6 : (a -> b -> p -> q -> r -> s -> t) -> Update a c e -> Update b c e -> Update p c e -> Update q c e -> Update r c e -> Update s c e -> Update t c e
-map6 f x y z a = ap << map5 f x y z a
+map6 f x y z a =
+    ap << map5 f x y z a
+
 
 join : Update (Update a c e) c e -> Update a c e
-join ( ( model, cmda, e ), cmdb, e2 ) = ( model, Cmd.batch [ cmda, cmdb ], e ++ e2 )
+join ( ( model, cmda, e ), cmdb, e2 ) =
+    ( model, Cmd.batch [ cmda, cmdb ], e ++ e2 )
+
 
 andThen : (b -> Update a c e) -> Update b c e -> Update a c e
-andThen f = join << map f
+andThen f =
+    join << map f
+
 
 kleisli : (b -> Update d c e) -> (a -> Update b c e) -> (a -> Update d c e)
-kleisli f g = andThen f << g
+kleisli f g =
+    andThen f << g
+
 
 andRunCmd : Cmd c -> Update a c e -> Update a c e
-andRunCmd = andThen << runCmd
+andRunCmd =
+    andThen << runCmd
+
 
 andInvokeHandler : e -> Update a c e -> Update a c e
-andInvokeHandler = andThen << invokeHandler
+andInvokeHandler =
+    andThen << invokeHandler
+
 
 foldEvents : Update a c (a -> Update a c e) -> Update a c e
-foldEvents ( m, cmd, events ) = List.foldr andThen ( m, cmd, [] ) events
+foldEvents ( m, cmd, events ) =
+    List.foldr andThen ( m, cmd, [] ) events
+
 
 andFinally : (m -> Update a c (a -> Update a c e)) -> Update m c (a -> Update a c e) -> Update a c e
-andFinally do = foldEvents << andThen do
+andFinally do =
+    foldEvents << andThen do
+
 
 applicationInit : (d -> e -> f -> Update a b c) -> d -> e -> f -> ( a, Cmd b )
-applicationInit f a b c = let ( model, cmd, _ ) = f a b c in ( model, cmd )
+applicationInit f a b c =
+    let
+        ( model, cmd, _ ) =
+            f a b c
+    in
+    ( model, cmd )
+
 
 documentInit : (f -> Update a b c) -> f -> ( a, Cmd b )
-documentInit f a = let ( model, cmd, _ ) = f a in ( model, cmd )
+documentInit f a =
+    let
+        ( model, cmd, _ ) =
+            f a
+    in
+    ( model, cmd )
+
 
 runUpdate : (d -> e -> Update a b c) -> d -> e -> ( a, Cmd b )
-runUpdate f a b = let ( model, cmd, _ ) = f a b in ( model, cmd )
+runUpdate f a b =
+    let
+        ( model, cmd, _ ) =
+            f a b
+    in
+    ( model, cmd )
--- src/Update/Deep/Browser.elm
+++ src/Update/Deep/Browser.elm
@@ -1,36 +1,44 @@
-module Update.Deep.Browser exposing (document, application)
+module Update.Deep.Browser exposing (application, document)
 
 import Browser exposing (Document, UrlRequest)
 import Browser.Navigation as Navigation
-import Update.Deep exposing (Update, documentInit, applicationInit, runUpdate)
+import Update.Deep exposing (Update, applicationInit, documentInit, runUpdate)
 import Url exposing (Url)
 
-application : { init          : flags -> Url -> Navigation.Key -> Update model msg a
-              , onUrlChange   : Url -> msg
-              , onUrlRequest  : UrlRequest -> msg
-              , subscriptions : model -> Sub msg
-              , update        : msg -> model -> Update model msg a
-              , view          : model -> Document msg
-              } -> Program flags model msg
-application config =
-  Browser.application
-    { init          = applicationInit config.init
-    , update        = runUpdate config.update
-    , subscriptions = config.subscriptions
-    , view          = config.view
-    , onUrlChange   = config.onUrlChange
-    , onUrlRequest  = config.onUrlRequest }
 
-document : { init          : flags -> Update model msg a
-           , onUrlChange   : Url -> msg
-           , onUrlRequest  : UrlRequest -> msg
-           , subscriptions : model -> Sub msg
-           , update        : msg -> model -> Update model msg a
-           , view          : model -> Document msg
-           } -> Program flags model msg
+application :
+    { init : flags -> Url -> Navigation.Key -> Update model msg a
+    , onUrlChange : Url -> msg
+    , onUrlRequest : UrlRequest -> msg
+    , subscriptions : model -> Sub msg
+    , update : msg -> model -> Update model msg a
+    , view : model -> Document msg
+    }
+    -> Program flags model msg
+application config =
+    Browser.application
+        { init = applicationInit config.init
+        , update = runUpdate config.update
+        , subscriptions = config.subscriptions
+        , view = config.view
+        , onUrlChange = config.onUrlChange
+        , onUrlRequest = config.onUrlRequest
+        }
+
+
+document :
+    { init : flags -> Update model msg a
+    , onUrlChange : Url -> msg
+    , onUrlRequest : UrlRequest -> msg
+    , subscriptions : model -> Sub msg
+    , update : msg -> model -> Update model msg a
+    , view : model -> Document msg
+    }
+    -> Program flags model msg
 document config =
-  Browser.document
-    { init          = documentInit config.init
-    , update        = runUpdate config.update
-    , subscriptions = config.subscriptions
-    , view          = config.view }
+    Browser.document
+        { init = documentInit config.init
+        , update = runUpdate config.update
+        , subscriptions = config.subscriptions
+        , view = config.view
+        }
