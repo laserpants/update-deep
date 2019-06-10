8650f1ae5265ef901694c15ee77295464202aa4d
Johannes Hildén <hildenjohannes@gmail.com>
2019-06-10 19:50:56 +0300

    run elm-format on tests

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,40 +1,58 @@
-module Main exposing (..)
+module Main exposing (suite, testJoin, testMap, testSave)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 import Update.Deep exposing (..)
 
+
 testSave : Test
 testSave =
-  let state = 5
-      ( a, _, e ) =
-        save state
-   in describe "save"
+    let
+        state =
+            5
+
+        ( a, _, e ) =
+            save state
+    in
+    describe "save"
         [ test "state" <|
             \_ -> Expect.equal a state
         , test "events" <|
-            \_ -> Expect.equal [] e ]
+            \_ -> Expect.equal [] e
+        ]
+
 
 testMap : Test
 testMap =
-  let state = 5
-      ( a, _, e ) =
-        map (\x -> x + 1) (save state)
-   in describe "map"
+    let
+        state =
+            5
+
+        ( a, _, e ) =
+            map (\x -> x + 1) (save state)
+    in
+    describe "map"
         [ test "this" <|
-            \_ -> Expect.equal a (state + 1) ]
+            \_ -> Expect.equal a (state + 1)
+        ]
+
 
 testJoin : Test
 testJoin =
-  let ( a, _, e ) =
-        join (save (save 5))
-   in describe "join"
+    let
+        ( a, _, e ) =
+            join (save (save 5))
+    in
+    describe "join"
         [ test "this" <|
-            \_ -> Expect.equal a 5 ]
+            \_ -> Expect.equal a 5
+        ]
+
 
 suite : Test
 suite =
-  describe "Update Deep"
-    [ testSave
-    , testMap ]
+    describe "Update Deep"
+        [ testSave
+        , testMap
+        ]
